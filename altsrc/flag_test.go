ba565b0da7585b4fafe1c7e00c653eb6602e5763
Igor Postelnik <igor@cloudflare.com>
2021-03-23 21:38:02 -0500

    Add ability to apply values from config file to hierarchical contexts, respecting per-command flag behavior

--- altsrc/flag_test.go
+++ altsrc/flag_test.go
@@ -496,3 +496,184 @@ func (p *Parser) Set(value string) error {
 func (p *Parser) String() string {
 	return fmt.Sprintf("%s,%s", p[0], p[1])
 }
+
+// TestApplyInputSource verifies that ApplyInputSource correctly respects
+// different hierarchical app/command/sub-command scenarios
+func TestApplyInputSource(t *testing.T) {
+	app := cli.App{
+		Name: "the-app",
+		Flags: []cli.Flag{
+			&cli.StringFlag{
+				Name: "simple",
+			},
+			NewStringFlag(&cli.StringFlag{
+				Name: "loadable",
+			}),
+			NewStringFlag(&cli.StringFlag{
+				Name: "loadable-shadow",
+			}),
+			&cli.StringFlag{
+				Name: "simple-app",
+			},
+			NewStringFlag(&cli.StringFlag{
+				Name: "loadable-app",
+			}),
+		},
+		Commands: []*cli.Command{
+			{
+				Name: "one",
+				Flags: []cli.Flag{
+					&cli.StringFlag{
+						Name: "simple",
+					},
+					NewStringFlag(&cli.StringFlag{
+						Name: "loadable",
+					}),
+					&cli.StringFlag{
+						Name: "loadable-shadow", // this make app's variable non-loadable
+					},
+					&cli.StringFlag{
+						Name: "simple-one",
+					},
+					NewStringFlag(&cli.StringFlag{
+						Name: "loadable-one",
+					}),
+				},
+			},
+			{
+				Name: "two",
+				Flags: []cli.Flag{
+					&cli.StringFlag{
+						Name: "simple",
+					},
+					NewStringFlag(&cli.StringFlag{
+						Name: "loadable",
+					}),
+					&cli.StringFlag{
+						Name: "override-two",
+					},
+					NewStringFlag(&cli.StringFlag{
+						Name: "loadable-two",
+					}),
+				},
+				Subcommands: []*cli.Command{
+					{
+						Name: "red",
+						Flags: []cli.Flag{
+							&cli.StringFlag{
+								Name: "loadable-shadow", // this shadows loadable variable in the app
+							},
+							&cli.StringFlag{
+								Name: "simple-red",
+							},
+							NewStringFlag(&cli.StringFlag{
+								Name: "override-two", // this will make 'override-two' loadable
+							}),
+						},
+					},
+				},
+			},
+		},
+	}
+
+	genSource := func(kv ...string) InputSourceContext {
+		m := make(map[interface{}]interface{})
+		for i := 0; i < len(kv); i += 2 {
+			m[kv[i]] = kv[i+1]
+		}
+		return NewMapInputSource("test-source", m)
+	}
+
+	tdata := []struct {
+		testCase string
+		source   InputSourceContext
+		args     string
+		check    []string
+		expected map[string]string
+	}{
+		{
+			testCase: "no args or file",
+			source:   genSource(),
+			args:     "app",
+			check:    []string{"simple", "loadable", "loadable-shadow"},
+			expected: make(map[string]string),
+		},
+		{
+			testCase: "app only",
+			source:   genSource("loadable", "APP", "loadable-shadow", "SHADOW", "simple", "SIMPLE"),
+			args:     "app",
+			check:    []string{"simple", "loadable", "loadable-shadow"},
+			expected: map[string]string{
+				"loadable":        "APP",
+				"loadable-shadow": "SHADOW",
+			},
+		},
+		{
+			testCase: "cmd one",
+			source:   genSource("loadable", "APP", "loadable-shadow", "SHADOW", "simple", "SIMPLE", "loadable-app", "APP"),
+			args:     "app one",
+			check:    []string{"simple", "loadable", "loadable-shadow", "loadable-app", "loadable-one"},
+			expected: map[string]string{
+				"loadable":        "APP",
+				"loadable-shadow": "", // non-loadable flag hides loadable flag at higher level
+				"loadable-app":    "APP",
+			},
+		},
+		{
+			testCase: "cmd two red",
+			source:   genSource("loadable", "APP", "loadable-shadow", "SHADOW", "simple", "SIMPLE", "loadable-app", "APP", "loadable-two", "TWO", "override-two", "RED"),
+			args:     "app two",
+			check:    []string{"simple", "loadable", "loadable-shadow", "loadable-app", "loadable-two", "override-two"},
+			expected: map[string]string{
+				"loadable":        "APP",
+				"loadable-shadow": "SHADOW", // subcommand "two" redefined loadable flag
+				"loadable-app":    "APP",
+				"loadable-two":    "TWO",
+				"override-two":    "", // subcommand "two" defines as non-loadable
+			},
+		},
+		{
+			testCase: "cmd two",
+			source:   genSource("loadable", "APP", "loadable-shadow", "SHADOW", "simple", "SIMPLE", "loadable-app", "APP", "override-two", "RED"),
+			args:     "app two red",
+			check:    []string{"simple", "loadable", "loadable-shadow", "loadable-app", "loadable-two", "override-two"},
+			expected: map[string]string{
+				"loadable":        "APP",
+				"loadable-shadow": "", // subcommand "red" redefined as non-loadable
+				"loadable-app":    "APP",
+				"override-two":    "RED", // subcommand "red" redefined as loadable
+			},
+		},
+	}
+
+	for _, test := range tdata {
+		t.Run(test.testCase, func(t *testing.T) {
+			app.Action = func(context *cli.Context) error {
+				if err := ApplyInputSource(context, test.source); err != nil {
+					t.Fatal(test.testCase, err)
+				}
+				for _, name := range test.check {
+					actual := context.String(name)
+					if actual != test.expected[name] {
+						t.Errorf("%s: expected %q got %q", name, test.expected[name], actual)
+					}
+				}
+
+				return nil
+			}
+			for _, command := range app.Commands {
+				command.Action = app.Action
+				for _, sub := range command.Subcommands {
+					sub.Action = app.Action
+				}
+			}
+
+			args := strings.Split(test.args, " ")
+			fmt.Println(args)
+			err := app.Run(args)
+			if err != nil {
+				t.Errorf("%s action returned %v", test.testCase, err)
+			}
+		})
+	}
+}
